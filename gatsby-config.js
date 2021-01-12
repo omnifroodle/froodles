fecce4f4e2ce0d99c68b873f4a486455f668c8d3
Matt Overstreet <matt.overstreet@gmail.com>
2021-01-12 20:22:42 +0000

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-WKY7XDBTZQ`,
       },
     },
     `gatsby-plugin-feed`,
